333abc89fee1ee2c41a6d952554d3b33c97f28ca
Salomi Margret M <266315236+margretmsalomi-official@users.noreply.github.com>
2026-04-06 13:15:37 +0530

    added json

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -37,3 +38,11 @@ elif ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+st.text(smoothiefroot_response.json())
+
+import pandas as pd
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
